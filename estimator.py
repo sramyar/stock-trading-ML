04c2090887e36c3929a424ce44ba181145ef0db0
Sepehr <sepehr.ramyar@gmail.com>
2019-11-27 10:13:08 -0800

    added GET_DATA function

--- estimator.py
+++ estimator.py
@@ -1,17 +1,20 @@
 class Estimator:
 
-    def __init__(estimator_type):
+    def __init__(self, estimator_type):
+        '''
+        Constructor that takes in the type of estimator as a parameter
+        '''
         if estimator_type == 'linreg':
             from sklearn.linear_model import LinearRegression
             self.learner = LinearRegression()
 
-    def trainer(start_date, end_date, tickers, data):
+    def get_data(self, start_date, end_date, tickers, data):
         '''
-        INPUT - start and end date for date, list of tickers, and the data set
+        INPUT - start and end dates as well as list of tickers for the data
 
-        OUTPUT - a dictionary mapping each ticker to its trained model
+        OUTPUT - a dictionary mapping the data for each corresponding ticker
         '''
-        train_data = {}
+        dataset = {}
         
         for ticker in tickers:
             try:
@@ -20,19 +23,31 @@ class Estimator:
                 frame = data[(data.ticker == ticker) & (data.date <= end_date) &
                 (data.date >= start_date)]
 
-                xy[0] = frame[['open', 'high', 'low', 'close', 'volume']]
+                xy.append(frame[['open', 'high', 'low', 'close', 'volume']])
                 
-                xy[1] = frame[['adj_close']]
+                xy.append(frame[['adj_close']])
 
+                dataset[ticker] = xy
             except:
                 print('No such date available in the dataset. Choose dates again.')
+                break
+        
+        return dataset
 
-        models = {}
+
+    def trainer(self, start_date, end_date, tickers, data):
+        '''
+        INPUT - start and end date for date, list of tickers, and the data set
+
+        OUTPUT - a list of (ticker, trained model) pairs
+        '''
+        train_data = self.get_data(start_date, end_date, tickers, data)
+
+        output = []
 
         for ticker in tickers:
             X, y = train_data[ticker][0], train_data[ticker][1]
-            models[ticker] = LinearRegression().fit(X,y)
+            model = LinearRegression().fit(X,y)
+            output.append((ticker, model))
         
-        return models
-
-        
+        return output
